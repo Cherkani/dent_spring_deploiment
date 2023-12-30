55e0501b1688a4fde8938e77846d15cca6942683
Cherkani <124716884+Cherkani@users.noreply.github.com>
2023-12-30 21:11:45 +0100

    Update AdminController.java

--- src/main/java/com/dent/dent/controllers/AdminController.java
+++ src/main/java/com/dent/dent/controllers/AdminController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
     @RequestMapping("/api/v1/admins")
-    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001","http://localhost:5173"})
+    @CrossOrigin(origins = "*")
 
     public class AdminController {
         @Autowired
